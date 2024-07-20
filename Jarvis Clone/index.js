efe80a4593ff51c5e73c25ea1c4d95b442ad645f
ahmadahad009 <ahmadahad283@gmail.com>
2024-07-21 01:03:06 +0530

    New File Added

---
+++ Jarvis Clone/index.js
@@ -0,0 +1,29 @@
+
+
+var menuBtn = document.querySelector(".ri-menu-line");
+var qty = 0;
+var nav = document.querySelector(".nav-center");
+menuBtn.addEventListener("click", function () {
+  if (qty == 0) {
+    nav.style.height = "0px";
+    nav.style.paddingTop = "0rem";
+    qty += 1;
+  } else if (qty == 1) {
+    nav.style.height = "100vh";
+    nav.style.paddingTop = "2rem";
+    qty = 0;
+  }
+
+  console.log(menuBtn);
+});
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
